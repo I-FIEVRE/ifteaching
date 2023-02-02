4ba5b190cea3c817b5c7038d94ca628e40febcc5
I-FIEVRE <fievre.isabelle@gmail.com>
2023-02-02 09:44:22 +0100

    changes and quotes

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -17,14 +17,14 @@ function output(listOfQuotes) {
 }
 
 //Create another function called getQuotes. Make it an async function.
-//In the function, using the built-in fetch method, call this absolute URL: 'https://run.mocky.io/v3/bf4e4d85-cda4-46c1-a433-4ecee6dd033f'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
+//In the function, using the built-in fetch method, call this absolute URL: 'https://raw.githubusercontent.com/I-FIEVRE/ifteaching/main/quotes.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
 //Convert the fetch response into a Javascript object ( hint: .json() ). Store this in the quoteList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.
 //Finally, call the output function and pass it the list of quotes. Execute the getquotes function to make sure it works correctly.
 async function getQuotes(){
-    const response = await fetch('https://run.mocky.io/v3/bf4e4d85-cda4-46c1-a433-4ecee6dd033f');
+    const response = await fetch('https://raw.githubusercontent.com/I-FIEVRE/ifteaching/main/quotes.json');
     if (response.ok) {
         listOfQuotes = await response.json();
-        output(listOfQuotes);
+        //output(listOfQuotes);
     }
 }
 getQuotes();
@@ -34,17 +34,31 @@ getQuotes();
 // - Calls the output function passing in the filtered list of quotes named arrayForDisplay
 function DisplayTempleFilter() {
     let element = document.getElementById("filterQuotes");
-    let order = document.getElementById("filterBy").value;
+    let choice = document.getElementById("filterBy").value;
     element.innerHTML = "";
     let arrayForDisplay;
-    if (order == "Unity") {
+    if (choice == "Unity") {
         arrayForDisplay = listOfQuotes.filter((quote) => {
             return quote.topic == "Unity";
         });     
-    } else {
+    } else if  (choice == "Peace") {
         arrayForDisplay = listOfQuotes.filter((quote) => {
-            return quote.topic != "Unity";
-        });
+            return quote.topic == "Peace";
+        });     
+    } else if (choice == "Love") {
+        arrayForDisplay = listOfQuotes.filter((quote) => {
+            return quote.topic == "Love";
+        });     
+    } else if (choice == "Positivity") {
+        arrayForDisplay = listOfQuotes.filter((quote) => {
+            return quote.topic == "Positivity";
+        });     
+    } else if (choice == "Work") {
+        arrayForDisplay = listOfQuotes.filter((quote) => {
+            return quote.topic == "Work";
+        });     
+    } else {
+        output(arrayForDisplay);
     }
     output(arrayForDisplay);
 }   
