72718c165eeee38e66ffbf1817a20f14fbca9e42
I-FIEVRE <fievre.isabelle@gmail.com>
2023-02-03 11:22:03 +0100

    changes for french version

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,6 +1,7 @@
 
 //global empty array variable to store a list of quotes
 let listOfQuotes = [];
+let listeDeCitations = [];
 
 //output accepts a list of quotes as an array argument and does the following for each quote:
 // - Creates an HTML <article> element
@@ -8,13 +9,15 @@ let listOfQuotes = [];
 // - Creates an HTML <p> element and add the quote's author property, the quote's talkTitle property, and the quote's year property to it
 // - Appends the <h4> element, and the <p> element to the <article> element as children
 // - Appends the <article> element to the HTML element with an ID of fitlerQuotes
-function output(listOfQuotes) {
-    let element = document.getElementById("filterQuotes");
+function output(listQuotes, filters) {
+    let element = document.getElementById(filters);
     //element.innerHTML = "";
-    listOfQuotes.forEach(quote => {element.innerHTML += `<article><h4>${quote.quoteContent}</h4>
+    listQuotes.forEach(quote => {element.innerHTML += `<article><h4>${quote.quoteContent}</h4>
     <p>${quote.author} - ${quote.talkTitle} (${quote.year})</p></article>`    
     });
 }
+
+
 
 //getQuotes, async function:
 //- uses the built-in fetch method, call this absolute URL: 'https://raw.githubusercontent.com/I-FIEVRE/ifteaching/main/scripts/quotes.json'. 
@@ -29,38 +32,55 @@ async function getQuotes(){
 }
 getQuotes();
 
+//getCitations is french version fo getQuotes
+async function getCitations(){
+    const response = await fetch('https://raw.githubusercontent.com/I-FIEVRE/ifteaching/main/scripts/quotes.json');
+    if (response.ok) {
+        listeDeCitations = await response.json();
+    }
+}
+getCitations();
+
 //displayQuoteFilter does the following:
 // - filters the global quote list by the currently selected value of the HTML element with an ID of filterBy
 // - Calls the output function passing in the filtered list of quotes named arrayForDisplay
-function displayTempleFilter() {
-    let element = document.getElementById("filterQuotes");
+function displayQuoteFilter(listQuotes,filters) {
+    let element = document.getElementById(filters);
     let choice = document.getElementById("filterBy").value;
     element.innerHTML = "";
     let arrayForDisplay;
     if (choice == "Unity") {
-        arrayForDisplay = listOfQuotes.filter((quote) => {
+        arrayForDisplay = listQuotes.filter((quote) => {
             return quote.topic == "Unity";
         });     
     } else if  (choice == "Peace") {
-        arrayForDisplay = listOfQuotes.filter((quote) => {
+        arrayForDisplay = listQuotes.filter((quote) => {
             return quote.topic == "Peace";
         });     
     } else if (choice == "Love") {
-        arrayForDisplay = listOfQuotes.filter((quote) => {
+        arrayForDisplay = listQuotes.filter((quote) => {
             return quote.topic == "Love";
         });     
     } else if (choice == "Positivity") {
-        arrayForDisplay = listOfQuotes.filter((quote) => {
+        arrayForDisplay = listQuotes.filter((quote) => {
             return quote.topic == "Positivity";
         });     
     } else if (choice == "Work") {
-        arrayForDisplay = listOfQuotes.filter((quote) => {
+        arrayForDisplay = listQuotes.filter((quote) => {
             return quote.topic == "Work";
         });     
     } else {
-        output(arrayForDisplay);
+        output(arrayForDisplay, filters);
+        
     }
-    output(arrayForDisplay);
-}   
-document.getElementById("btnLoad").addEventListener('click', displayTempleFilter);
+    output(arrayForDisplay, filters);
+}  
+function displayQ() {
+    displayQuoteFilter(listOfQuotes, "filterQuotes");
+}; 
+function displayC() {
+    displayQuoteFilter(listeDeCitations, "filterCitations");
+};
+document.getElementById("btnLoad").addEventListener('click', displayQ);
+document.getElementById("btnLoad").addEventListener('click', displayC);
 
